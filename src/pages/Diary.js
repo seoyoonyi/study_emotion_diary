73d55b3f441b230ace0832b589bffbd8ce982f13
seoyoonyi <yiseoyoon@gmail.com>
2022-07-23 11:30:57 +0900

    220723 router 응용 공부

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Diary = () => {
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div>
       <h2>여기는 Diary입니다</h2>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get('id');
+  console.log('id : ', id);
+
+  const mode = searchParams.get('mode');
+  console.log('mode : ', mode);
+
   return (
     <div>
       <h2>여기는 Edit입니다</h2>
+      <button onClick={() => setSearchParams({ who: 'winterlood' })}>
+        바꾸기
+      </button>
+      <button onClick={() => navigate('/home')}>HOME으로 가기</button>
+      <button onClick={() => navigate(-1)}>뒤로 가기</button>
     </div>
   );
 };
